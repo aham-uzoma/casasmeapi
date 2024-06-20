11e41cf46ca1d50246e39fef4e35acc7d279575b
aham-uzoma <ahamefula.uzoma@gmail.com>
2024-06-20 20:24:32 +0100

    edit allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,6 @@ const allowedOrigins = [
     'http://casasme.com.ng',
     'https://casasme.onrender.com',
     'http://casasme.onrender.com',
-    'http://localhost:3000',
 ]
 
 module.exports = allowedOrigins
